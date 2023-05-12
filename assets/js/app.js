473f72ac7e188c4509f632cc835b3fd630ff66f1
karenvp <karenvalierpalmero@gmail.com>
2023-05-12 10:32:37 -0600

    feat: refactorización de codigo para consumir una api de usuarios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,33 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+//Cambie el nombre de las variables a unos mas descriptivos, y arregle las referencias de las clases name y blog en el query selector
+const userName = document.querySelector('.name');
+const userBlog = document.querySelector('.blog');
+const locationUser = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+
+//Esta funcion te permite hacer una peticion get a una api de usuarios, segun el usuario que le indiques.
+//Agregue la palabra reservada async a la funcion
+async function displayUser(username) {
+
+  //Agregue un try catch para manejar los errores
+  try {
+    //Cambie los textContent por innerHTML
+    userName.innerHTML = 'cargando...';
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    //Asigne a una constante data la respuesta de la peticion y lo converti a objeto js
+    const data = await response.json();
+    //Hice desestructuracion del objeto para acceder a los valores solicitados
+    const {name, blog, location} = data;
+    userName.innerHTML = `${name}`;
+    userBlog.innerHTML = `${blog}`;
+    locationUser.innerHTML = `${location}`;
+  } catch (error) {
+    console.log('OH NO!');
+    console.log(error);
+    userName.textContent = `Algo salió mal: ${error}`
+  }
+ 
 }
 
-function handleError(err) {
-  console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
-}
-
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski')
